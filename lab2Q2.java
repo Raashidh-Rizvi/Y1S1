22823644d2f5dd042b94ca0bcacce43645905e3f
Raashidh2002 <raashidh24@gmail.com>
2025-01-14 20:46:42 +0530

    Add files via upload

---
+++ lab2Q2.java
@@ -0,0 +1,33 @@
+public class lab2Q2{
+
+public static void main (String[]args) {
+
+/* A rope is used to create a square fence. The length of a side of the square is known
+ (assume it is 10). Later this same rope is used to create a circular fence.
+
+ Write a Java program to find the radius of the circular fence*/
+
+
+	int l= 10;
+	double p= 4 * l;
+	double r= p / (2 * (Math.PI));
+
+	System.out.println("Radius Of the Circular Fence : " + r );
+
+}
+
+}
+
+
+	
+	
+
+
+
+	
+	
+	
+	
+	
+
+	
