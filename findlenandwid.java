3814e85b2b922eefe0b529a2a3dd80cfbf1373ae
Raashidh2002 <raashidh24@gmail.com>
2025-01-14 19:58:46 +0530

    Add files via upload

---
+++ findlenandwid.java
@@ -0,0 +1,25 @@
+public class findlenandwid {
+
+public static void main(String[]args) {
+
+	
+	
+	Double p ,w,l;
+	
+	p=100.0;
+	l=2*p/7;
+        w=(3*l)/4;
+	
+	
+	System.out.println ("Length of the fence : "+l);
+
+	System.out.println("Width of the fence : "+w);
+
+}
+
+}
+	
+	
+	
+
+
