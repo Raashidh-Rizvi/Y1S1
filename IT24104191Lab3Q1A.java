a07a6ef1ef428c46fb1565a6c293b73961e35e52
Raashidh2002 <raashidh24@gmail.com>
2025-01-14 21:43:36 +0530

    Add files via upload

---
+++ IT24104191Lab3Q1A.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class IT24104191Lab3Q1A {
+
+public static void main (String[]args) {
+
+/* Enter the price of 1kg of rice and the number of kilograms you want to buy from the
+ keyboard. Write a Java program to find the amount you have to pay. */
+
+
+ Scanner input = new Scanner(System.in);
+	System.out.println();
+
+	System.out.print ("Enter The Price Of 1kg of rice : ");
+	double price = input.nextInt();
+
+	System.out.print ("Enter The Number of Kilograms You Want to buy : " );
+	double kilos = input.nextInt();
+
+	double total = price * kilos;
+	System.out.println();
+
+	System.out.print("The total amount is : " + total );
+	System.out.println();
+
+}
+
+}
+	
+	
+
+	
+
+
