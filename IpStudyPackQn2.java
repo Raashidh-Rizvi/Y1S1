941e04fcd1da492f0232edd0a8542a19c9464bc5
Raashidh2002 <raashidh24@gmail.com>
2025-01-26 19:22:19 +0530

    Add files via upload

--- IpStudyPackQn2.java
+++ IpStudyPackQn2.java
@@ -1,3 +1,36 @@
+/*Question (02) 
+Write a java program to find out the amount you earn after investing money on a trust fund for a given period. 
+a) Write a method called calcAnnualInterest() to calculate the interest amount the customer gets at 
+the end of the year when the annual interest rate and the amount is passed as parameters. If the amount is 
+above  
+Rs. 1,000,000.00, an additional 0.5% is added to the interest rate. 
+float calcAnnualInterest(float interestRate, float amount); 
+interest = amount * interest rate /100 
+ 
+b) Write a method called findTotalAmount() to return the total amount saved at the end of the year. 
+float findTotalAmount(float interestRate, float amount); 
+Hint: Total amount = interest + amount 
+Use calcAnnualInterest function to find out the interest earned. 
+ 
+c) Write a method called testTotalAmount() which contains two assert statements to check the 
+function implemented in section b) above. 
+void testTotalAmount(); 
+d) In your main method, 
+i) Call testTotalAmount() method 
+ii) Input the amount to be invested and the annual interest rate from the keyboard. 
+iii) Customer is planning to invest the amount compounded yearly at the given interest rate for five years. 
+i.e. (Amount + interest) is invested for the next year. 
+Display the total amount earned at the end of each year using the above methods. 
+ 
+Sample output given below  
+Enter Initial Amount to be invested : 10000.00 
+Enter Annual interest Rate(in percentage) : 10 
+Amount after year 1 : 11000.00 
+Amount after year 2 : 12100.00  
+Amount after year 3 : 13310.00 
+*/
+
+
 import java.util.Scanner;
 
 public class IpStudyPackQn2 {
